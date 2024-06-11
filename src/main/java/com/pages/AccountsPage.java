bf93674085293a3e9f6bfb4bb4198cbbe4023a8d
Srinivasachari Kamsali <k.srinivasachari@gmail.com>
2024-06-11 09:53:51 +0530

    ec5

--- src/main/java/com/pages/AccountsPage.java
+++ src/main/java/com/pages/AccountsPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class AccountsPage {
-//sample comment --edited in Github--edited eclipse3--eclip4
+//sample comment --edited in Github--edited eclipse3--eclip4--ec5
 	private WebDriver driver;
 
 	private By accountSections = By.cssSelector("div#center_column span");
